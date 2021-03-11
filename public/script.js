74538f17fc4c67ae85006f662bc2cc0aeae562e8
Daynamarie <Dayna.christison@gmail.com>
2021-03-11 16:43:57 -0500

    port is port 3000

--- public/script.js
+++ public/script.js
@@ -2,3 +2,38 @@
 //runs everytime the page is loaded
 
 console.log('ello')
+
+//define the variables that reference elements on our page
+const foodsList = document.getElementById("foods");
+const foodsForm = document.querySelector("form");
+
+//add a helper function that creates a list item for a given food 
+function appendNewFood(food){
+    const newListItem = document.createElement('li');
+    newListItem.innerText = food;
+    foodsList.appendChild(newListItem);
+}
+
+//fetch the initial list of dreams
+fetch("/foods")
+    .then(response => response.json()) //parse the json from the server
+    .then(foods => {
+        //remove the loading text
+        foodsList.firstElementChild.remove();
+
+        //iterate through every food and add it to our page
+        foods.forEach(appendNewFood);
+
+        //listen for the form to be submitted and add a new food when it is
+        foodsForm.addEventListener('submit', event => {
+            //stop our submission from refreshing the page
+            event.preventDefault();
+            //get food value and add it to the list
+            let newFood = foodsForm.elements.food.value;
+            foods.push(newFood);
+            appendNewFood(newFood);
+
+            foodsForm.reset();
+            foodsForm.elements.food.focus();
+        });
+    });
---
+++ server.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+const PORT = process.env.PORT || 3000
+const app = express();
+
+console.log('dis node')
+
+const foods = [
+    'burger',
+    'fries',
+    'pizza'
+];
+
+//make all of the files in public available
+app.use(express.static("public"));
+
+//get req to home page and then a function
+app.get("/", function(req, res){
+    //.sendFile transfers the file at the hiven path and it sets the Content-Type response HTTP header fiels based on the file name extension
+    res.sendFile(_dirname + "/index.html")
+});
+
+//send the default array to the page
+app.get("/foods", function(req, res){
+    // express helps us tak JS objects and send them as json
+    res.json(foods)
+})
+
+//listen for requests
+app.listen(PORT, () => {
+    console.log(`your app is listening on ${PORT}`);
+});
