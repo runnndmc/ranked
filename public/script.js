c95cbff802d20cc4f25ee3d9f6d24c9ee438ba09
Daynamarie <Dayna.christison@gmail.com>
2021-03-15 16:18:20 -0400

    added delete functionality

--- public/script.js
+++ public/script.js
@@ -3,37 +3,57 @@
 
 console.log('ello')
 
-//define the variables that reference elements on our page
-const foodsList = document.getElementById("foods");
-const foodsForm = document.querySelector("form");
 
-//add a helper function that creates a list item for a given food 
+//define the variables that reference elements on our page
+const foodsUnorderedList = document.querySelector("#foods-ul");
+const foodsInputForm = document.querySelector(".add-new-input-form");
+
+//add a helper function that creates a list item for a newly inputted food 
 function appendNewFood(food){
+    //create a new element 
     const newListItem = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    //have the new element have the text of the input given
     newListItem.innerText = food;
-    foodsList.appendChild(newListItem);
+    deleteButton.innerText = "delete"
+
+    newListItem.append(deleteButton)
+    //grab the parent of the list item and append the new element created 
+    foodsUnorderedList.appendChild(newListItem);
+    deleteButton.addEventListener('click', deleteListItem)
+    return deleteButton
 }
 
-//fetch the initial list of dreams
+
+function deleteListItem(){
+    this.parentNode.remove()
+}
+
+
+
+//fetch the initial list of foods in server.js
 fetch("/foods")
     .then(response => response.json()) //parse the json from the server
     .then(foods => {
         //remove the loading text
-        foodsList.firstElementChild.remove();
-
+        foodsUnorderedList.firstElementChild.remove();
         //iterate through every food and add it to our page
         foods.forEach(appendNewFood);
 
         //listen for the form to be submitted and add a new food when it is
-        foodsForm.addEventListener('submit', event => {
+        foodsInputForm.addEventListener('submit', event => {
+
             //stop our submission from refreshing the page
             event.preventDefault();
-            //get food value and add it to the list
-            let newFood = foodsForm.elements.food.value;
+
+            //get food value 
+            let newFood = foodsInputForm.elements.food.value;
+            //and add it to the list
             foods.push(newFood);
             appendNewFood(newFood);
 
-            foodsForm.reset();
-            foodsForm.elements.food.focus();
+            foodsInputForm.reset();
+            foodsInputForm.elements.food.focus();
         });
+       
     });
